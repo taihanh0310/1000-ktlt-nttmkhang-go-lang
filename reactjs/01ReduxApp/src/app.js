eef310e6e5e70db797dbcf49ac191e8ee2622d1a
Hanh Nguyen <taihanh0310@gmail.com>
2018-05-22 07:13:42 +0700

    update-delete reducer

--- reactjs/01ReduxApp/src/app.js
+++ reactjs/01ReduxApp/src/app.js
@@ -17,6 +17,40 @@ const reducer = function(state = {books: []}, action){
 			return {books: [...state.books, ...action.payload]} // add action.payload in collection
 			break;
 		}
+		// Delete book
+		case 'DELETE_BOOK': {
+			// create a copy of the current array of books
+			const currentBookToDelete = [...state.books];
+
+			// determine at which index in array is the book to be delete
+			const indexToDelete = currentBookToDelete.findIndex(function(book){
+				return book.id == action.payload.id;
+			})
+
+			// use slide to remove the book at the specified index
+			return {books: [...currentBookToDelete.slice(0, indexToDelete), 
+				...currentBookToDelete.slice(indexToDelete + 1)]};
+			break;
+		}
+
+		case 'UPDATE_BOOK': {
+			const currentBookToUpdate = [...state.books];
+
+			const indexToUpdate = currentBookToUpdate.findIndex(function(book){
+				return book.id === action.payload.id;
+			});
+
+			const newBookTpUpdate = {
+				...currentBookToUpdate[indexToUpdate],
+				title: action.payload.title
+			};
+
+			console.log("what is it newBookTpUpdate", newBookTpUpdate);
+
+			return {books: [...currentBookToUpdate.slice(0, indexToUpdate), newBookTpUpdate, 
+				...currentBookToUpdate.slice(indexToUpdate + 1)]};
+			break;
+		}
 	}
 	return state;
 }
@@ -62,3 +96,19 @@ store.dispatch({
 	}]
 	
 })
+
+store.dispatch({
+	type: 'DELETE_BOOK',
+	payload: {id: 3}
+})
+
+store.dispatch({
+	type: 'UPDATE_BOOK',
+	payload: {
+		id: 2,
+		title: 'title new update 2'
+	}
+})
+
+// delete a book
+
