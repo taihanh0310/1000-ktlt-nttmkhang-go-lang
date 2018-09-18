c91daab4bbfa995b8d73f743979a83db64884554
Tri Pham <t.phaminh@drcomgroup.com>
2018-09-18 13:34:38 +0700

    update

---
+++ swift/udemy/01 Dice App/01 Dice App/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  01 Dice App
+//
+//  Created by Drcom Asia on 9/18/18.
+//  Copyright © 2018 Drcom Asia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    let arrImage: Array = ["dice1","dice2", "dice3", "dice4", "dice5", "dice6"]
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func btnRoll(_ sender: UIButton) {
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        diceImageView1.image = UIImage(named: arrImage[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: arrImage[randomDiceIndex2])
+//        print(randomDiceIndex1)
+    }
+}
+
