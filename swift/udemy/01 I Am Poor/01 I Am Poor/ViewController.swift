15ef44365574f69e6a31fdf6cf9a812a22a272a9
Tri Pham <t.phaminh@drcomgroup.com>
2018-09-18 08:51:03 +0700

    01 i am poor

---
+++ swift/udemy/01 I Am Poor/01 I Am Poor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  01 I Am Poor
+//
+//  Created by Drcom Asia on 9/18/18.
+//  Copyright © 2018 Drcom Asia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
