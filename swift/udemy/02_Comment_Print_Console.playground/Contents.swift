6da8a40c883ba65827f8fe168264acc9ad791653
Hanh Nguyen <taihanh0310@gmail.com>
2018-09-19 06:36:02 +0700

    update test ex

---
+++ swift/udemy/02_Comment_Print_Console.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print(str)
+
+var tmpCreate: Int = 20
+tmpCreate = tmpCreate * 20
+print("tmp create 1: ", tmpCreate)
+tmpCreate = tmpCreate / 90
+print("tmp create 2: ", tmpCreate)
+tmpCreate = tmpCreate + 19
+
+print(tmpCreate)
