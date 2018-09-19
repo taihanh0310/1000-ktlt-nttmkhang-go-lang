15e8f89dd1897ae7b5e7797c6a5ec576e5b1e8d4
Tri Pham <t.phaminh@drcomgroup.com>
2018-09-19 09:07:15 +0700

    update

---
+++ swift/udemy/02 BMI Calculator/02 BMI Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  02 BMI Calculator
+//
+//  Created by Drcom Asia on 9/19/18.
+//  Copyright © 2018 Drcom Asia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
