437bed1d58e0ba02dcf7d28ef5f12d29d7815d43
Tri Pham <t.phaminh@drcomgroup.com>
2018-10-03 18:50:07 +0700

    updartew

--- swift/01.BasicSyntax/01.BasicSyntax/main.swift
+++ swift/01.BasicSyntax/01.BasicSyntax/main.swift
@@ -25,33 +25,34 @@ import Foundation
 
 print("Control flow")
 print("01: Each")
-let individualScore = [75,23,45,46,8,23]
-var teamScore = 0
+//let individualScore = [75,23,45,46,8,23]
+//var teamScore = 0
+//
+//for score in individualScore {
+//    if(score > 50){
+//        teamScore += 3
+//    }
+//    else{
+//        teamScore += 1
+//    }
+//}
+//
+//print(teamScore)
+//
+//print("Kiem tra option")
+//var optionString: String? = "Hello"
+//print(optionString == nil)
+//
+//var optionName: String? = "John Appleased"
+//
+//print("Toan tu 3 ngoi")
+//let nickName: String? = nil
+//let fullName: String = "Hanh Nguyen"
+//// kiem tra nickname = null thi in ra default value
+//let informationGreeting = "Hi \(nickName ?? fullName)"
+//
+//print(informationGreeting)
+//
+//print("===================SWITCH================")
 
-for score in individualScore {
-    if(score > 50){
-        teamScore += 3
-    }
-    else{
-        teamScore += 1
-    }
-}
-
-print(teamScore)
-
-print("Kiem tra option")
-var optionString: String? = "Hello"
-print(optionString == nil)
-
-var optionName: String? = "John Appleased"
-
-print("Toan tu 3 ngoi")
-let nickName: String? = nil
-let fullName: String = "Hanh Nguyen"
-// kiem tra nickname = null thi in ra default value
-let informationGreeting = "Hi \(nickName ?? fullName)"
-
-print(informationGreeting)
-
-print("===================SWITCH================")
-
+print((3, "apple") < (3, "bird"))
