5abf246f1b852cfc8e00435765e990b8e867ed71
hanh nguyen <taihanh0310@gmail.com>
2018-11-19 20:15:16 +0700

    collection view examole

---
+++ swift/udemy/06-collectionView/06-collectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  06-collectionView
+//
+//  Created by Hanh Nguyen on 11/19/18.
+//  Copyright © 2018 Hanh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
