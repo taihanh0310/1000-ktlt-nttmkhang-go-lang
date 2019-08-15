df78c86b47601707fda6c9ad88fd75efe5f5f106
hanh nguyen <taihanh0310@gmail.com>
2019-08-16 05:41:54 +0700

    update

---
+++ documents/MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var str = "Hello, playground"
+str = "Hello work"
+
+print(str)
+
+// variables
+
+var x:Int = 10
+var y:Int = 20
+var z:Int = 0
+var submarineName:String = "Tuoitre"
+
+z = x + y
+
+print(z)
+// ----------------------excercies page 48 ---------------
+//Write code within a Swift playground that multiplies two integers and displays the result.
+
+//Write code within a Swift playground that squares a float. Display the resulting float.
+//Write code within a Swift playground that subtracts two floats, with the result being stored as an integer. Note that rounding does not occur.
+// ----------------------end excercies page 48 ---------------
